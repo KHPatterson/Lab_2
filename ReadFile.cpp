28cfdaf3aeab25ce5612bbd47e1161b239805962
Kristen Patterson <becauseyousaidplease@gmail.com>
2016-09-06 15:40:14 -0500

    test commit

--- ReadFile.cpp
+++ ReadFile.cpp
@@ -3,44 +3,40 @@
 #include <string>
 using namespace std; 
 
+
 ReadFile* ReadFile::createReadFile(const char* file_name)
 {
-   ReadFile* rf = new ReadFile;
-
-   rf->input_file.open(file_name);
-   rf->closed = false;
-   rf->_eof = false;
-
-   return rf;
+   ReadFile* rf;
+   input_file.open(file_name);
+   closed = false;
+   _eof = false;
 }
 
-ReadFile::~ReadFile();
+ReadFile::~ReadFile()
 {
-   close(rf);
-   delete rf;
 }
 
 bool ReadFile::eof()
 {
-   return rf->_eof;
+   return _eof;
 }
 
 void ReadFile::close()
 {
-   if (!rf->closed)
+   if (!closed)
    {
-      rf->input_file.close();
-      rf->closed = true;
+      input_file.close();
+      closed = true;
    }
 }
 
 String* ReadFile::readLine()
 {
-   if (rf->closed) return NULL;
-   if (rf->_eof) return NULL;
+   if (closed) return NULL;
+   if (_eof) return NULL;
 
    string text;
-   rf->_eof = !(getline(rf->input_file, text));
+   _eof = !(getline(input_file, text));
 
    String* str = new String((const char*) text.c_str());
    return str;
--- ReadFile.h
+++ ReadFile.h
@@ -8,19 +8,23 @@ using namespace std;
 
 class ReadFile
 {
+		
 	public:
 		ifstream input_file;
 		bool _eof;
 		bool closed;
-	 
-		ReadFile();  //Constructor.
-		~ReadFile();  //Deconstructor.
 		
+		ReadFile* rf;
+		
+	 	ReadFile();  //Constructor.
+		~ReadFile();  //Deconstructor.
+
 		ReadFile* createReadFile(const char* file_name);
 		void destroyReadFile();
 		String* readLine();
 		bool eof();
 		void close();
+
 };
 
 #endif
